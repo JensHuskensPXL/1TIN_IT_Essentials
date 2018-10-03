14d978e70200839d46f9c11833d50e4b0df43f89
JensHuskensPXL <jens.huskens@student.pxl.be>
2018-10-03 11:00:26 +0200

    Oefening 4.4 Verbeterd

--- IT-Essentials-Oefeningen/4_Iteraties/oefening4.2.py
+++ IT-Essentials-Oefeningen/4_Iteraties/oefening4.2.py
@@ -4,4 +4,4 @@ for i in range(1,21):
     tabel = i * geheel_getal
     print(i, "x", geheel_getal, "=", tabel)
 
-# print("{:2} x {:2} = {:4}".format(i,geheel_getal, tabel)) 
+# print("{:2} x {:2} = {:4}".format(i,geheel_getal, tabel))
--- IT-Essentials-Oefeningen/4_Iteraties/oefening4.4.py
+++ IT-Essentials-Oefeningen/4_Iteraties/oefening4.4.py
@@ -1,13 +1,24 @@
-gewicht_aarde = int(input("Geef je gewicht in: "))
+# gewicht_aarde = int(input("Geef je gewicht in: "))
+#
+# for i in ("aarde", "maan", "jupiter", "mars"):
+#     if i == "aarde":
+#         factor = 1
+#     elif i == "maan":
+#         factor = 0.165
+#     elif i == "jupiter":
+#         factor = 2.537
+#     else:
+#         factor = 0.378
+#     gewicht = factor * gewicht_aarde
+#     print(i + ":", gewicht)
 
-for i in ("aarde", "maan", "jupiter", "mars"):
-    if i == "aarde":
-        factor = 1
-    elif i == "maan":
-        factor = 0.165
-    elif i == "jupiter":
-        factor = 2.537
-    else:
-        factor = 0.378
-    gewicht = factor * gewicht_aarde
-    print(i + ":", gewicht)
+maan = float(input("Geef de percentag gewicht van de maan tov aarde nl 16.5: "))
+jupiter =
+mars =
+
+for i in range(50,125,5):
+    print("Aarde:", i)
+    print("Maan:", i * maan / 100)
+    print("Jupiter:" , i * jupiter / 100)
+    print("Mars:", i * mars / 100)
+    print()
