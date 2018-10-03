46ca3b6ef6cb580e12ccf8cee8e2f346c5b9bbd6
JensHuskensPXL <jens.huskens@student.pxl.be>
2018-10-03 10:55:56 +0200

    Oefeningen Verbeterd

--- IT-Essentials-Oefeningen/4_Iteraties/oefening4.2.py
+++ IT-Essentials-Oefeningen/4_Iteraties/oefening4.2.py
@@ -1,5 +1,7 @@
 geheel_getal = int(input("Geef een geheel getal in: "))
 
 for i in range(1,21):
-    tabel = i * 7
+    tabel = i * geheel_getal
     print(i, "x", geheel_getal, "=", tabel)
+
+# print("{:2} x {:2} = {:4}".format(i,geheel_getal, tabel)) 
